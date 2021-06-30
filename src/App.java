667796a9854b82a5613840d3ebbb449dddea3c0b
Eduar24601 <egdinho90@gmail.com>
2021-06-29 21:00:40 -0500

    second classs

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil; 
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // tipos de datos y operadores
+        int resultado = 15 + 32;
+        String res = "Hola";
+        double resultado2 = 25 / 3;
+        float resultado4 = 58 * 21;
+        int resultado5 = 21 % 9;
+        //operadores unarios
+        resultado++;
+        resultado--;
+        resultado = -resultado;
+        resultado = +resultado;
+        //operadores logicos
+        boolean respuesta = true;
+        respuesta = 5 > 8;
+        respuesta = 5 < 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 != 8;
+        respuesta = 5 == 8;
+        respuesta = 12 < 5 && 4 > 1;
+        respuesta = 14 < 25 || 1 > 5;
+
+
     }
 }
